3ddaab7a1168cb8d6d4f3a676513ebc6e7acbe56
Ryan Zielke <NeoAlchemy@gmail.com>
2023-12-01 16:05:41 -0600

    browser to server call for data
    created a basic browser to server call for data

--- src/app/main.py
+++ src/app/main.py
@@ -9,6 +9,11 @@ def index():
     return render_template('main.html', name="Ryan")
 
 
+@app.route('/day-1')
+def day1():
+   app.logger.debug('day 1')
+   return "day 1"
+
 
 if __name__ == "__main__":
   app.run(debug=True)
